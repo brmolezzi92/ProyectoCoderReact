fccdbd7e3435f013dd30dc0c6ba4a71f27b6f003
Bruno Molezzi <bru.molezzi92@gmail.com>
2022-12-27 17:29:18 -0300

    Final

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Navbar from './componentes/Navbar/Navbar';
-import ItemListContainer from "./componentes/ItemContainer/ItemListContainer"
+import ItemListContainer from "./componentes/ItemListContainer/ItemListContainer"
 
 function App() {
   return (
